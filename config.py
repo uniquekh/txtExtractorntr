1c45e76170e7f4e65a35dfc505754598a148545e
Unique <mujeem000@gmail.com>
2024-06-27 08:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6618274486:AAESKaMTJJ0tOR62pqOPt-a0WcfpTi0y7nk")
-    API_ID = int(os.environ.get("API_ID", "20089019"))
-    API_HASH = os.environ.get("API_HASH", "0b2eb6aa72d5983fa9e4b8c77791c9c4")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6404676836")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7158015873:AAFNXF3OEfexth-SAnpQ6Foa9XGhY6HeqAU")
+    API_ID = int(os.environ.get("API_ID", "21179966"))
+    API_HASH = os.environ.get("API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "7326397503")
